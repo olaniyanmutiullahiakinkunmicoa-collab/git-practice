ef8b7f8c0fde606f4109e90bec42200eacb349df
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-07-14 21:53:38 +0100

    Add Java CI pipeline

---
+++ src/Main.c
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to GitHub Actions!");
+
+        int a = 20;
+        int b = 15;
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
