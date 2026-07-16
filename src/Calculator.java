ca73086a987c9cba961048bfde836f579b22aa85
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-07-16 23:16:25 +0100

    the claculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,11 @@
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a - b;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
 }
