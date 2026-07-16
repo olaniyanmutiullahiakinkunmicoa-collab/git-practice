659c0bb2649dc691ac6007460868d0df69af44e8
coachakin <olaniyanmutiullahiakinkunmicoa@gmail.com>
2026-07-16 23:13:38 +0100

    the claculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,11 @@
-public class Main {
+public class Calculator {
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to GitHub Actions!");
-
-        int a = 20;
-        int b = 15;
-
-        System.out.println("Sum = " + (a + b));
+    public int add(int a, int b) {
+        return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
 }
